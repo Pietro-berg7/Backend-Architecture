6de608d07564206ccb10f42ead9f9b25e2f89354
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-09-03 19:17:38 -0300

    Modelando as entidades de banco de dados

---
+++ CourseAPI/Infrastructure/Data/CursoDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CourseAPI.Infrastructure.Data
+{
+    public class CursoDbContext : DbContext
+    {
+    }
+}
---
+++ CourseAPI/Infrastructure/Data/Mappings/CursoMapping.cs
@@ -0,0 +1,15 @@
+﻿using CourseAPI.Business.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CourseAPI.Infrastructure.Data.Mappings
+{
+    public class CursoMapping: IEntityTypeConfiguration<Curso>
+    {
+        public void Configure(EntityTypeBuilder<Curso> builder)
+        {
+            builder.ToTable("TB_CURSO");
+            builder.HasKey(p => p.Codigo)
+        }
+    }
+}
